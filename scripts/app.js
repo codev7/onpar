91108e266780f95b85acfacae086b3824c88e0d6
robert <caking7@hotmail.com>
2016-02-08 17:48:32 +0200

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,102 @@
+var HOST_DIRECTORY = "/onpartv/service/";
+var DOWNLOAD_LIMIT_TIME = 24; // when  24 hour , new download // slide.html page
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name realApp
+ * @description
+ * # realApp
+ *
+ * Main module of the application.
+ */
+angular
+        .module('realApp', [
+            'ngAnimate',
+            'ngRoute',
+			'ui.bootstrap',
+			'videosharing-embed',
+			'bootstrapLightbox'
+        ])
+        .config(function ($routeProvider) {
+            $routeProvider
+                    .when('/', {
+                        templateUrl: 'views/main.html',
+                        controller: 'MainCtrl',
+                        controllerAs: 'main'
+                    })
+                    .when('/slide', {
+                        templateUrl: 'views/slide.html',
+                        controller: 'SlideCtrl',
+                        controllerAs: 'slide'
+                    })
+                    .when('/crawl', {
+                        templateUrl: 'views/crawl.html',
+                        controller: 'crawlCtrl',
+                        controllerAs: 'crawl'
+                    })
+                    .when('/userinfo', {
+                        templateUrl: 'views/userinfo.html',
+                        controller: 'UserinfoCtrl',
+                        controllerAs: 'userinfo'
+                    })
+                    .when('/login', {
+                        templateUrl: 'views/login.html',
+                        controller: 'LoginCtrl',
+                        controllerAs: 'login'
+                    })
+                    .when('/fileupload', {
+                        templateUrl: 'views/fileupload.html',
+                        controller: 'FileuploadCtrl',
+                        controllerAs: 'fileupload'
+                    })
+                    .when('/schedule', {
+                        templateUrl: 'views/schedule.html',
+                        controller: 'ScheduleCtrl',
+                        controllerAs: 'schedule'
+                    })
+                    .when('/slideshow', {
+                        templateUrl: 'views/slideshow.html',
+                        controller: 'SlideshowCtrl',
+                        controllerAs: 'slideshow'
+                    })
+					.when('/viewschedule', {
+                        templateUrl: 'views/viewschedule.html',
+                        controller: 'ViewscheduleCtrl',
+                        controllerAs: 'viewschedule'
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
+        });
+
+/////////////////////////
+//  RootController
+//  
+/////////////////////////
+angular.module("realApp").controller("RootController", function ($scope, $window, $location) {
+//	console.log($window.sessionStorage.login);
+    // Route Change Watch
+    $scope.$on('$routeChangeStart', function () {
+        if ($window.sessionStorage.login === "success") {
+            $scope.isLogin = true;
+        }
+        else {
+            $scope.isLogin = false;
+        }
+    });
+    $scope.logout = function(){
+        $window.sessionStorage.login = "";
+        $window.sessionStorage.first_name = "";
+        $window.sessionStorage.last_name = "";
+        $window.sessionStorage.user_id = "";
+        $window.sessionStorage.user_email = "";
+        $window.sessionStorage.user_password = "";
+    }
+    $scope.isActive = function(viewLocation) {
+        return viewLocation===$location.path();
+    }
+});
+
+
